8b5e09c45dbbd5e2b875d0302024fde49b272b38
Slava Tretyak <s.tretyak@dev.powermemobile.com>
2012-10-16 17:56:02 +0300

    18818 ModCaseinsensitive

--- src/RG.ModCaseInsensitive/Module.cs
+++ src/RG.ModCaseInsensitive/Module.cs
@@ -4,35 +4,42 @@ using System.Web;
 using System.Collections.Generic;
 using System.IO;
 
-namespace RG.ModCaseInsensitive {
-	public class Module : IHttpModule {
-		public void Dispose () {}
-		
-		private CaseMap _CM = null;
-		
-		public void Init ( HttpApplication app ) {
-			Common.Log( "ModCaseInsensitive:Init( app -> {0} )", app );
-			app.BeginRequest += OnBeginRequest;
-			
-			AppDomainSetup setup = AppDomain.CurrentDomain.SetupInformation;
-			string baseDir = setup.ApplicationBase;
-			
-			_CM = new CaseMap(baseDir, HttpContext.Current.Request.ApplicationPath);
-			_CM.BuildIndex();
-		}
-		public void OnBeginRequest ( object sender, EventArgs ea ) {
-			Common.Log( "ModCaseInsensitive:OnBeginRequest ( sender -> {0}, ea -> {1} )", sender, ea );
-			var ctx = HttpContext.Current;
-			
-			var path = ctx.Request.RawUrl;
-			//var newPath = _CM.Resolve( path );
-			//Common.Log("ModCaseInsensitive: rewritting [ '{0}' -> '{1}' ]", path, newPath);
-			//ctx.RewritePath( newPath );
+namespace RG.ModCaseInsensitive
+{
+    public class Module : IHttpModule
+    {
+        public void Dispose() { }
 
-			var rd = _CM.Resolve( path );
+        private static CaseMap _CM = null;
 
-			rd.CondRewrite(ctx);			
-		} 
-	}
+        static Module()
+        {
+            AppDomainSetup setup = AppDomain.CurrentDomain.SetupInformation;
+            string baseDir = setup.ApplicationBase;
+
+            _CM = new CaseMap(baseDir, HttpContext.Current.Request.ApplicationPath);
+            _CM.BuildIndex();
+        }
+
+        public void Init(HttpApplication app)
+        {
+            Common.Log("ModCaseInsensitive:Init( app -> {0} )", app);
+            app.BeginRequest += OnBeginRequest;
+        }
+        public void OnBeginRequest(object sender, EventArgs ea)
+        {
+            Common.Log("ModCaseInsensitive:OnBeginRequest ( sender -> {0}, ea -> {1} )", sender, ea);
+            var ctx = HttpContext.Current;
+
+            var path = ctx.Request.RawUrl;
+            //var newPath = _CM.Resolve( path );
+            //Common.Log("ModCaseInsensitive: rewritting [ '{0}' -> '{1}' ]", path, newPath);
+            //ctx.RewritePath( newPath );
+
+            var rd = _CM.Resolve(path);
+
+            rd.CondRewrite(ctx);
+        }
+    }
 }
 
