e2015f2f950e1c3f39b5fce062a073d5b30e2a73
Slava Tretyak <s.tretyak@dev.powermemobile.com>
2017-09-19 19:03:05 +0300

    added mode clear logging

--- src/mod_caseinsensitive/RoutingDecision.cs
+++ src/mod_caseinsensitive/RoutingDecision.cs
@@ -14,24 +14,35 @@ namespace mod_caseinsensitive
 			QueryString = "";
 		}
 
-		public void CondRewrite(HttpContext ctx) {
-			if ( RewriteNeeded ) {
-				Common.Log("ModCaseInsensitive: rewritting [ '{0}' -> '{1}' ]", 
-					ctx.Request.RawUrl,
-					Url + PathInfo + QueryString
-				);
+        public void CondRewrite(HttpContext ctx)
+        {
+            if (RewriteNeeded)
+            {
+                Common.Log("ModCaseInsensitive: rewritting [ '{0}' -> Url: {1}, PathInfo: {2}, QueryString: {3} ]",
+                    ctx.Request.RawUrl,
+                    Url, PathInfo, QueryString
+                );
 
-				if ( PathInfo == String.Empty ) {
-					ctx.RewritePath( Url + QueryString );
-				} 
-				else {
-					ctx.RewritePath( Url, PathInfo, QueryString );
-				}
-			}
-			else {
-				Common.Log("ModCaseInsensitive: not rewritting [ '{0}' ]", ctx.Request.RawUrl);
-			}
-		}
+                if (PathInfo == String.Empty)
+                {
+                    if (Url.EndsWith("/") || QueryString.StartsWith("/"))
+                        ctx.RewritePath(Url + QueryString);
+                    else
+                        ctx.RewritePath(Url + "/" + QueryString);
+                }
+                else
+                {
+                    Common.Log("ModCaseInsensitive : rewritting ctx.RewritePath(Url, PathInfo, QueryString)");
+                    ctx.RewritePath(Url, PathInfo, QueryString);
+                    //ctx.RewritePath(Url + "/" + PathInfo + QueryString);
+                    //ctx.RewritePath(Url + "/" + PathInfo, "", QueryString);
+                }
+            }
+            else
+            {
+                Common.Log("ModCaseInsensitive: not rewritting [ '{0}' ]", ctx.Request.RawUrl);
+            }
+        }
 		
 		public bool RewriteNeeded { get; set; }
 		public string Url { get; set; }
